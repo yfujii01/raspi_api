e55dff7eda07706eb1be43f1558199935926013f
yfujii01 <yfujii01@gmail.com>
2020-03-27 22:20:42 +0900

    fix console.log

--- routes/shell.js
+++ routes/shell.js
@@ -3,18 +3,9 @@ var router = express.Router();
 const execSync = require('child_process').execSync;
 
 router.get('/', function(req, res, next) {
-  const result =  execSync('ls ./');
-  execSync('bash ./script/script1.bash');
-
-  console.log(req);
-  console.log(req.query);
   const query = req.query;
-
-  execSync('bash ./script/script2.bash ' + query.aaa);
   execSync('bash ./script/say.sh hoge ' + query.aaa);
-
-  res.send(query);
-  // res.send('this is /shell index!' + result);
+  res.send('ok');
 });
 
 module.exports = router;
